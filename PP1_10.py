2eb50e4aa5d3b9c4ad208166969bba1430e165a8
DhakshayiniChandrasekaran-ICD2O1-01 <dhakshayini.chandrasekaran@student.tdsb.on.ca>
2024-11-05 21:25:35 +0000

    hi

--- PP1_10.py
+++ PP1_10.py
@@ -1,8 +1,8 @@
 '''
-    Lesson: Math Library
-    Author: Dhakshayini
-    Date Created: October 2, 2024
-    Date Last Modified: October 2, 2024
+  Lesson: Math Library
+  Author: Dhakshayini
+  Date Created: October 2, 2024
+  Date Last Modified: October 2, 2024
 '''
 
 def q1(): 
@@ -10,30 +10,29 @@ def q1():
   user = float(input("Input a number: "))
   print(math.isqrt(user))
   
-  
 
 def q2(): 
   #Write Assignment code here
-user = int(input("Input a number: "))
-print(math.floor(user))
+  user = int(input("Input a number: "))
+  print(math.floor(user))
 
 def q3(): 
   #Write Assignment code here
-user = float(input("Input a number: "))
-print(math.floor(user))
+  user = float(input("Input a number: "))
+  print(math.floor(user))
 
 def q4(): 
   #Write Assignment code here
-user = float(input("Input a number: "))
-print(math.ceil(user))
+  user = float(input("Input a number: "))
+  print(math.ceil(user))
 
 def q5(): 
   #Write Assignment code here
-user = float(input(Input a number: "))
-user 2 = float(input("Input another number: "))
-user *=user 2
-user = math.trunc(user/2)
-print(user)
+  user = float(input("Input a number: "))
+  user2 = float(input("Input another number: "))
+  user *= user2
+  user = math.trunc(user/2)
+  print(user)
 
 
 #Do not alter the following code
